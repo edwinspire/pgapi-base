26ad7848bdc31dfc6f4348e60f31cd376fe5dcc9
Edwin De La Cruz <edwinspire@gmail.com>
2021-02-28 22:36:47 -0500

    Modificación en Fetch

--- components/edwinspire/UrlSearch.js
+++
@@ -1,7 +0,0 @@
-import { onMount } from 'svelte';
-
-export function UrlSearch(search) {
-  let searchParams = new URLSearchParams(new URL(window.location.href).search);
-  console.log(search);
-  return searchParams.get(search);
-}
