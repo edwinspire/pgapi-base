9e40daae69fe5cbe556319898e3d518d628f9253
Edwin De La Cruz <edwinspire@gmail.com>
2021-04-12 18:12:41 -0500

    Se quita  is-fixed-top del menú

--- src/components/edwinspire/Menu/Menu.svelte
+++ src/components/edwinspire/Menu/Menu.svelte
@@ -22,7 +22,7 @@
     });
   </script>
   
-  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+  <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="/home">
         <span class="icon">
