d85df0ef8b5b1560b04977b7b1e538cb37dcf608
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-11-13 02:43:37 -0500

    Agrega WithSession component

---
+++ src/svelte/components/svelt-session/WithSession.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  var { CheckSession } = require("./Preload");
+  export function preload(page, session) {
+    return CheckSession(this, page, session, "/");
+  }
+</script>
+
+<div>
+  <slot name="content" />
+</div>
