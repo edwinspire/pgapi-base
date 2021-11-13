944bf15dc10ecd5222ba8dcf67ef7f2f173f6178
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-11-13 11:46:27 -0500

    Se elimina WithSession component

--- src/svelte/components/svelt-session/Preload.js
+++
@@ -1,8 +0,0 @@
-// Esta función sirve para validar si el usuario está logueado para permitir el acceso a la ruta, caso contrario le devuelve a la pantalla de login
-export function CheckSession(module, page, session, url_redirect_if_no_session) {
-  if (session && session.user) {
-    return { page: page, session: session }
-  } else {
-    return module.redirect(302, url_redirect_if_no_session);
-  }
-}
---
+++ src/svelte/components/svelt-session/Session.js
@@ -0,0 +1,12 @@
+const { REDIRECT_ON_UNAUTHORIZED } = process.env;
+import { UserSession } from "./Store";
+
+export function RequireSession(module, page, session) {
+  if (session && session.user) {
+    UserSession.set(session.user);
+    return { page: page, session: session };
+  } else {
+    UserSession.set({});
+    return module.redirect(401, REDIRECT_ON_UNAUTHORIZED || "/UNAUTHORIZED");
+  }
+}
