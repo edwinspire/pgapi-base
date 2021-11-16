fa1bb7e6b4ffb895fd74e540646a1ee465b06fca
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-11-15 22:10:02 -0500

    Agrega fetch aun Session para registrar o consultar una página.

--- src/svelte/svelt-session/Session.js
+++ src/svelte/svelt-session/Session.js
@@ -5,7 +5,7 @@ import { uFetch } from "@edwinspire/universal-fetch/src/fetch";
 export async function RequireSession(module, page, session) {
   console.log("RequireSession", module, page, session);
   var FData = new uFetch();
-  let resp = await FData.get(`https://localhost:${PORT}/pgapi_v1/gui/check/path`, { page, session });
+  let resp = await FData.post(`https://localhost:${PORT}/pgapi_v1/gui/check/path`, { page, session });
   let resp_path = await resp.json();
 
   console.log("RequireSession", resp_path);
