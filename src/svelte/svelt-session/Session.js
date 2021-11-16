1996cbc4b5c1698c51335006d6a352403d45fa5c
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-11-15 21:36:47 -0500

    Agrega fetch aun Session para registrar o consultar una página

--- src/svelte/svelt-session/Session.js
+++ src/svelte/svelt-session/Session.js
@@ -1,8 +1,12 @@
 const { REDIRECT_ON_UNAUTHORIZED } = process.env;
 import { UserSession } from "./Store";
-//import {uFetch} from "@edwinspire/universal-fetch/src/fetch";
+import {uFetch} from "@edwinspire/universal-fetch/src/fetch";
 
-export function RequireSession(module, page, session) {
+export async function RequireSession(module, page, session) {
+console.log("RequireSession", module, page, session);
+  //var FData = new uFetch();
+  //let resp = await FData.get(`/api/session/${session}`);
+
   if (session && session.user) {
     UserSession.set(session.user);
     return { page: page, session: session };
