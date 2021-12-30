0872da28ec9530386c573b7174bdc8ff918ca0df
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-12-29 21:13:40 -0500

    Se agrega notificación de cambio en las tablas por websocket. Bug

--- src/server/Server.js
+++ src/server/Server.js
@@ -26,6 +26,7 @@ export class Server extends EventEmitter {
     this.credentials = credentials;
     this.cluster = cluster;
     this.AccessPoint = new AccessPoint(custom_response);
+    this.listen_notification_list = listen_notification_list;
 /*
     if (listen_notification_list && listen_notification_list.length > 0) {
       new pgListen(listen_notification_list).on("notification", (notify) => {
@@ -111,8 +112,8 @@ export class Server extends EventEmitter {
       //      httpServer.timeout = EXPRESSJS_SERVER_TIMEOUT||120000;
       this.socketio = SocketIO(httpServer);
 
-      if (listen_notification_list && listen_notification_list.length > 0) {
-        new pgListen(listen_notification_list).on("notification", (notify) => {
+      if (this.listen_notification_list && this.listen_notification_list.length > 0) {
+        new pgListen(this.listen_notification_list).on("notification", (notify) => {
           this.emit("pgNotify", notify);
           this.socketio.emit("pg-change-table", notify.payload);
         });
