342251ba518d4d051eb18ebe35e2e672ef353e83
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-01-03 22:50:28 -0500

    CSe elimina Telegraf

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,4 +1,5 @@
-const { PORT, NODE_ENV, TOKEN_ENCRYPT, EXPRESSJS_SERVER_TIMEOUT, PGAPI_DEBUG } = process.env;
+const { PORT, NODE_ENV, TOKEN_ENCRYPT, EXPRESSJS_SERVER_TIMEOUT, PGAPI_DEBUG } =
+  process.env;
 const dev = NODE_ENV === "development";
 import sirv from "sirv";
 import compression from "compression";
@@ -6,13 +7,14 @@ const AccessPoint = require("./class/fnAccessPoint.js").AccessPoint;
 import GeneralRoutes from "./class/routes";
 const EventEmitter = require("events");
 const express = require("express");
-var methodOverride = require('method-override');
+var methodOverride = require("method-override");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const { pgListen } = require("./class/pgListen");
 const { SocketIO } = require("./class/SocketIO");
 const { Token } = require("./class/Tokendb");
-var bodyParser = require('body-parser')
+//const { Telegraf } = require("./class/Telegraf");
+var bodyParser = require("body-parser");
 
 export class Server extends EventEmitter {
   constructor({
@@ -27,7 +29,7 @@ export class Server extends EventEmitter {
     this.cluster = cluster;
     this.AccessPoint = new AccessPoint(custom_response);
     this.listen_notification_list = listen_notification_list;
-/*
+    /*
     if (listen_notification_list && listen_notification_list.length > 0) {
       new pgListen(listen_notification_list).on("notification", (notify) => {
         this.emit("pgNotify", notify);
@@ -42,38 +44,14 @@ export class Server extends EventEmitter {
     this.app.use(morgan("dev"));
     this.app.use(methodOverride());
     this.app.use(cookieParser(TOKEN_ENCRYPT));
-    this.app.use(bodyParser.json({ strict: false, limit: '100mb' })); //-- Limit 100M
+    this.app.use(bodyParser.json({ strict: false, limit: "100mb" })); //-- Limit 100M
     this.app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
-    this.app.use(bodyParser({limit: '100mb'}));
-    /*
-    this.app.use(
-      session({
-        secret: TOKEN_ENCRYPT,
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-          maxAge: 2 * 60 * 60 * 1000, // 1 hour
-          httpOnly: true,
-          //secure: false, // Uncomment this line to enforce HTTPS protocol.
-          sameSite: true,
-        },
-      })
-    );
-    */
-    /*
-    this.app.all("/pgapi*", async (req, res) => {
-      fnAccessPoint(req, res, custom_response);
-    });
-*/
-
-    //console.log(this.AccessPoint.Middleware);
+    this.app.use(bodyParser({ limit: "100mb" }));
 
     this.app.use((req, res, next) => {
       this.AccessPoint.Middleware(req, res, next);
     });
 
-    //this.app.use(passport.initialize());
-    //require("./class/Passport");
     this.app.use(GeneralRoutes);
 
     this.app.use(
@@ -112,13 +90,18 @@ export class Server extends EventEmitter {
       //      httpServer.timeout = EXPRESSJS_SERVER_TIMEOUT||120000;
       this.socketio = SocketIO(httpServer);
 
-      if (this.listen_notification_list && this.listen_notification_list.length > 0) {
-        new pgListen(this.listen_notification_list).on("notification", (notify) => {
-          this.emit("pgNotify", notify);
-          this.socketio.emit("pg-change-table", notify.payload);
-        });
+      if (
+        this.listen_notification_list &&
+        this.listen_notification_list.length > 0
+      ) {
+        new pgListen(this.listen_notification_list).on(
+          "notification",
+          (notify) => {
+            this.emit("pgNotify", notify);
+            this.socketio.emit("pg-change-table", notify.payload);
+          }
+        );
       }
-
 
       httpServer.on("error", (e) => {
         console.trace(e);
